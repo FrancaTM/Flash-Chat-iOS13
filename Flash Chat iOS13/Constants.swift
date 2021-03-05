b54efc57d242811a5897478b7a3d981262e4f397
FrancaTM <francatm@outlook.com>
2021-03-05 08:29:20 -0300

    add constants file

---
+++ Flash Chat iOS13/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  Flash Chat iOS13
+//
+//  Created by Tulio Marcos Franca on 05/03/21.
+//  Copyright © 2021 Angela Yu. All rights reserved.
+//
+
+struct Constants {
+    static let registerSegue = "RegisterToChat"
+    static let loginSegue = "LoginToChat"
+    
+}
